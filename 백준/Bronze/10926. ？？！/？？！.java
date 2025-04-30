97982c5a24367af3f5d738979f12294710137d41
hhlee7 <gdjlhh777@gmail.com>
2025-04-30 14:02:09 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A;
+		A = sc.next();
+		A += "??!";
+		System.out.println(A);
+	}
+}
