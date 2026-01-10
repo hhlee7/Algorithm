dd37d529c6eb5be17130276f9a0233be8813b833
hhlee7 <gdjlhh777@gmail.com>
2026-01-11 00:03:50 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.length;
+        int last = num_list[len - 1];
+        int prev = num_list[len - 2];
+        
+        int[] answer = new int[len + 1];
+        
+        for(int i = 0; i < len; i++) {
+            answer[i] = num_list[i];
+        }
+        
+        if(last > prev) {
+            answer[len] = last - prev;
+        } else {
+            answer[len] = last * 2;
+        }
+                
+        return answer;
+    }
+}
