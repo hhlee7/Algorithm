51ba4b36ecda25a40c4d89a8449e26e136951d23
hhlee7 <gdjlhh777@gmail.com>
2026-03-26 23:56:47 +0900

    [level 1] Title: 폰켓몬, Time: 5.28 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,17 +1,6 @@
-import java.util.HashSet;
-
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int num : nums) {
-            set.add(num);
-        }
-        
-        answer = Math.min(set.size(), nums.length / 2);
-            
         return answer;
     }
 }
